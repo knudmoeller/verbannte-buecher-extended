4df76d7ba8a82119bd23e46d4540106e5465ee3c
Knud Möller <knud@datalysator.com>
2018-12-18 13:13:35 +0100

    generalize to_turtle.sh script

--- bin/to_turtle.sh
+++ bin/to_turtle.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-DATA=$1
-echo $DATA
+INPUT=$1
+OUTPUT=$2
+BASE=$3
 
-riot --syntax=jsonld $DATA/target/verbannte_buecher_neu.json | \
+riot --syntax=jsonld $INPUT | \
     rapper -i ntriples -o turtle \
     -f 'xmlns:schema="http://schema.org/"' \
     -f 'xmlns:dct="http://purl.org/dc/terms/"' \
     -f 'xmlns:owl="http://www.w3.org/2002/07/owl#"' \
-    -f 'xmlns:verbannt="https://daten.berlin.de/sites/default/files/ref/verbannt.ttl#"' \
-    -f 'xmlns:entry="https://daten.berlin.de/sites/default/files/data/verbannte_buecher/entry/"' \
-    -f 'xmlns:location="https://daten.berlin.de/sites/default/files/data/verbannte_buecher/location/"' \
-    -f 'xmlns:person="https://daten.berlin.de/sites/default/files/data/verbannte_buecher/person/"' \
-    --input-uri https://daten.berlin.de/sites/default/files/data/verbannte_buecher/ - > \
-    $DATA/target/verbannte_buecher_neu.ttl
+    -f 'xmlns:verbannt="$BASE/ref/verbannt.ttl#"' \
+    -f 'xmlns:entry="$BASE/data/verbannte_buecher/entry/"' \
+    -f 'xmlns:location="$BASE/data/verbannte_buecher/location/"' \
+    -f 'xmlns:person="$BASE/data/verbannte_buecher/person/"' \
+    --input-uri $BASE/data/verbannte_buecher/ - > \
+    $OUTPUT
