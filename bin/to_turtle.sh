0a24197502349a7702ed1d48ed2f1ac7d8f0bb91
Knud Möller <knud@datalysator.com>
2018-12-18 14:02:38 +0100

    add foaf ns to turtle conversion script

--- bin/to_turtle.sh
+++ bin/to_turtle.sh
@@ -7,6 +7,7 @@ BASE=$3
 riot --syntax=jsonld $INPUT | \
     rapper -i ntriples -o turtle \
     -f 'xmlns:schema="http://schema.org/"' \
+    -f 'xmlns:foaf=""http://xmlns.com/foaf/0.1/"' \
     -f 'xmlns:dct="http://purl.org/dc/terms/"' \
     -f 'xmlns:owl="http://www.w3.org/2002/07/owl#"' \
     -f 'xmlns:verbannt="$BASE/ref/verbannt.ttl#"' \
