12b4b6335badb032c570a38f357acf6230ffee0a
Knud Möller <knud@datalysator.com>
2018-12-19 12:06:21 +0100

    fix handling of base URI in turtle transformation script

--- bin/to_turtle.sh
+++ bin/to_turtle.sh
@@ -7,12 +7,14 @@ BASE=$3
 riot --syntax=jsonld $INPUT | \
     rapper -i ntriples -o turtle \
     -f 'xmlns:schema="http://schema.org/"' \
-    -f 'xmlns:foaf=""http://xmlns.com/foaf/0.1/"' \
+    -f 'xmlns:foaf="http://xmlns.com/foaf/0.1/"' \
     -f 'xmlns:dct="http://purl.org/dc/terms/"' \
     -f 'xmlns:owl="http://www.w3.org/2002/07/owl#"' \
-    -f 'xmlns:verbannt="$BASE/ref/verbannt.ttl#"' \
-    -f 'xmlns:entry="$BASE/data/verbannte_buecher/entry/"' \
-    -f 'xmlns:location="$BASE/data/verbannte_buecher/location/"' \
-    -f 'xmlns:person="$BASE/data/verbannte_buecher/person/"' \
-    --input-uri $BASE/data/verbannte_buecher/ - > \
+    -f "xmlns:verbannt='$BASE/ref/verbannt.ttl#'" \
+    -f "xmlns:entry='$BASE/data/verbannte_buecher/entry/'" \
+    -f "xmlns:location='$BASE/data/verbannte_buecher/location/'" \
+    -f "xmlns:person='$BASE/data/verbannte_buecher/person/'" \
+    --input-uri $BASE \
+    --output-uri - \
+    - > \
     $OUTPUT
